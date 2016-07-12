5000d5bde973961212ba3c45ab53eab127ec46d3
valeye <projectvaleye@gmail.com>
2016-07-11 22:14:18 -0700

    Update server.js

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -2,14 +2,14 @@
 // ======================================
 
 // CALL THE PACKAGES --------------------
-var express    = require('express');		// call express
-var app        = express(); 				// define our app using express
-var bodyParser = require('body-parser'); 	// get body-parser
-var morgan     = require('morgan'); 		// used to see requests
-var mongoose   = require('mongoose');
-var config 	   = require('./config');
-var path 	   = require('path');
-var cors       = require('cors');
+var express    	= require('express');		// call express
+var app        	= express(); 			// define our app using express
+var bodyParser 	= require('body-parser'); 	// get body-parser
+var morgan     	= require('morgan'); 		// used to see requests
+var mongoose   	= require('mongoose');
+var config 	= require('./config');
+var path 	= require('path');
+var cors       	= require('cors');
 
 // APP CONFIGURATION ==================
 // ====================================
