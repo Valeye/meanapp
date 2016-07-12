54e2ec0be3a426dbc402297cff7be6041f9ed3e4
valeye <projectvaleye@gmail.com>
2016-07-11 22:04:30 -0700

    Create server.js

---
+++ nodefe/server.js
@@ -0,0 +1,30 @@
+// BASE SETUP
+// ======================================
+
+// CALL THE PACKAGES --------------------
+var express    = require('express');		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser'); 	// get body-parser
+var morgan     = require('morgan'); 		// used to see requests
+var mongoose   = require('mongoose');
+var config 	   = require('./config');
+var path 	   = require('path');
+
+// log all requests to the console 
+app.use(morgan('dev'));
+
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
+// MAIN CATCHALL ROUTE --------------- 
+// SEND USERS TO FRONTEND ------------
+// has to be registered after API ROUTES
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+// START THE SERVER
+// ====================================
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
